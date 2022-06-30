6ff42921ce03d0bb61bde102b5c817359e16d2b0
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-30 16:01:19 +0200

    Dodaj pliki projektów.

---
+++ Zadanie5.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad._5._2
+{
+    class Program
+    {
+        static int Przedzial(int a, int b, int x)
+        {
+
+            if (a < x && x < b) return 1;
+            else return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, x;
+
+            Console.WriteLine("Podaj 'a': ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj 'b': ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj 'x': ");
+            x = int.Parse(Console.ReadLine());
+            if (Przedzial(a, b, x) == 0)
+                Console.WriteLine("x nie nalezy do przedzialu otwartego");
+            else Console.WriteLine("x NALEZY do przedzialu");
+            Console.ReadKey();
+        }
+    }
+}
